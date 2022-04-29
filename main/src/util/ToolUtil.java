763cd39aa8cfb4be9139dfae0a5d9a535ccbae5b
YuXing <simon.lin@tpisoftware.com>
2022-04-30 00:41:49 +0800

    feat : ToolUtil (some useful method)

---
+++ main/src/util/ToolUtil.java
@@ -0,0 +1,13 @@
+package util;
+
+/**
+ * @author Simon
+ */
+public class ToolUtil {
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
